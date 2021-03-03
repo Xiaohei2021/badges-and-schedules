cf988ffd0801adfb7fd60f4f8cc6d14d802e8725
Leon <hualiang.tian@gmail.com>
2021-03-02 22:40:49 -0500

    commit tree 9f0238f78a8362f8b446fd2aad0d5399dd2d98bc

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+require 'pry'
+
+def badge_maker(speaker_name)
+    return "Hello, my name is #{speaker_name}."
+end
+
+def batch_badge_creator(speaker_list)
+    attendee_badges = []
+    speaker_list.each {|attendee| attendee_badges << "Hello, my name is #{attendee}." }
+#binding.pry   
+    return attendee_badges
+end
+
+def assign_rooms(speaker_list)
+    room_arrangement = []
+    speaker_list.each_with_index {|attendee, index| room_arrangement << "Hello, #{attendee}! You'll be assigned to room #{index+1}!"}
+    return room_arrangement
+end
+
+def printer(speaker_list)
+    #binding.pry
+    batch_badge_creator(speaker_list).each {|attendee| puts attendee }
+    assign_rooms(speaker_list).each {|attendee| puts attendee }
+    
+end
+
